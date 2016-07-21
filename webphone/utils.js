2df5be2af458bfc52bfebab14eb0d491f1e678d2
iboam <iboam@yahoo.es>
2016-07-21 10:52:26 -0400

    Add files via upload

--- webphone/utils.js
+++ webphone/utils.js
@@ -1,180 +1,63 @@
-function getWebAppVersion() {
-    return '1.1.13';
+/* config of the plugin with the links for downloads and the version of the plugin */
+function getConfig(){  
+	var version = '1.0.14';
+	var config = {
+		version : version,
+		files: {
+			'Windows' : {
+				'x86' : {
+					'Explorer' : {
+						file: 'http://web.linphone.org/downloads/linphone-web-' + version + '-Linux-x86.xpi',
+						icon: 'style/img/linphone.ico'
+					},
+					'DEFAULT' : 'http://web.linphone.org/downloads/linphone-web-' + version + '-Win32.msi'
+				},
+				'x86_64' : {
+					'Explorer' : {
+						file: 'http://web.linphone.org/downloads/linphone-web-' + version + '-Linux-x86.xpi',
+						icon: 'style/img/linphone.ico'
+					},
+					'DEFAULT' : 'http://web.linphone.org/downloads/linphone-web-' + version + '-Win32.msi'
+				}
+			},
+			'Linux' : {
+				'x86' : {
+					'Firefox' : {
+						file: 'http://web.linphone.org/downloads/linphone-web-' + version + '-Linux-x86.xpi',
+						icon: 'style/img/linphone.ico'
+					},
+					'DEFAULT' : 'http://web.linphone.org/downloads/linphone-web-' + version + '-Linux-x86.tar.gz'
+				}, 
+				'x86_64' : {
+					'Firefox' : {
+						file: 'http://web.linphone.org/downloads/linphone-web-' + version + '-Linux-x86_64.xpi',
+						icon: 'style/img/linphone.ico'
+					},
+					'DEFAULT' : 'http://web.linphone.org/downloads/linphone-web-' + version + '-Linux-x86_64.tar.gz'
+				}
+			},
+			'Mac' : {
+				'x86' : {
+					'Firefox' : {
+						file: 'http://web.linphone.org/downloads/linphone-web-' + version + '-Mac-x86.xpi',
+						icon: 'style/img/linphone.ico'
+					},
+					'DEFAULT' : 'http://web.linphone.org/downloads/linphone-web-' + version + '-Mac-x86.pkg'
+				}, 
+				'x86_64' : {
+					'Firefox' : {
+						file: 'http://web.linphone.org/downloads/linphone-web-' + version + '-Mac-x86.xpi',
+						icon: 'style/img/linphone.ico'
+					},
+					'DEFAULT' : 'http://web.linphone.org/downloads/linphone-web-' + version + '-Mac-x86.pkg'
+				}
+			}
+		}
+	};
+	return config;
 }
 
-function __linphone_init(a) {
-    var b = "1.0.14",
-        c = b.replace(/\./g, ","),
-        d = !1,
-        e = {
-            files: {
-                Windows: {
-                    x86: {
-                        Explorer: {
-                            file: "http://web.linphone.org/downloads/linphone-web-" + b + "-Win32.cab",
-                            version: c
-                        },
-                        DEFAULT: "http://web.linphone.org/downloads/linphone-web-" + b + "-Win32.msi"
-                    },
-                    x86_64: {
-                        Explorer: {
-                            file: "http://web.linphone.org/downloads/linphone-web-" + b + "-Win32.cab",
-                            version: c
-                        },
-                        DEFAULT: "http://web.linphone.org/downloads/linphone-web-" + b + "-Win32.msi"
-                    }
-                },
-                Linux: {
-                    x86: {
-                        Firefox: {
-                            file: "http://web.linphone.org/downloads/linphone-web-" + b + "-Linux-x86.xpi",
-                            icon: "http://web.linphone.org/style/img/linphone.ico"
-                        },
-                        DEFAULT: "http://web.linphone.org/downloads/linphone-web-" + b + "-Linux-x86.tar.gz"
-                    },
-                    x86_64: {
-                        Firefox: {
-                            file: "http://web.linphone.org/downloads/linphone-web-" + b + "-Linux-x86_64.xpi",
-                            icon: "http://web.linphone.org/style/img/linphone.ico"
-                        },
-                        DEFAULT: "http://web.linphone.org/downloads/linphone-web-" + b + "-Linux-x86_64.tar.gz"
-                    }
-                },
-                Mac: {
-                    x86: {
-                        Firefox: {
-                            file: "http://web.linphone.org/downloads/linphone-web-" + b + "-Mac-x86_64.xpi",
-                            icon: "http://web.linphone.org/style/img/linphone.ico"
-                        },
-                        DEFAULT: "http://web.linphone.org/downloads/linphone-web-" + b + "-Mac-x86_64.pkg"
-                    },
-                    x86_64: {
-                        Firefox: {
-                            file: "http://web.linphone.org/downloads/linphone-web-" + b + "-Mac-x86_64.xpi",
-                            icon: "http://web.linphone.org/style/img/linphone.ico"
-                        },
-                        DEFAULT: "http://web.linphone.org/downloads/linphone-web-" + b + "-Mac-x86_64.pkg"
-                    }
-                }
-            },
-            name: "Linphone Web",
-            version: b,
-            webapp_version: getWebAppVersion(),
-            mimetype: "x-linphone-web",
-            copyright: "Copyright© Belledonne Communications 2013. All rights reserved.",
-            linphone_account: {
-                cls: "createAccount",
-                link: "https://www.linphone.org/eng/linphone/register-a-linphone-account.html"
-            },
-            links: [{
-                cls: "support",
-                text: "support",
-                link: "https://www.linphone.org/eng/linphone/support.html"
-            }, {
-                cls: "sales",
-                text: "sales",
-                link: "http://www.belledonne-communications.com/contact.html"
-            }, {
-                cls: "license ",
-                text: "license",
-                link: "http://www.gnu.org/licenses/agpl-3.0.html"
-            }],
-            appLinks: {
-                windows_phone: {
-                    cls: "windows_phone",
-                    text: "windows_phone",
-                    appLink: "http://www.windowsphone.com/fr-FR/store/app/linphone/99661466-8c5c-489b-a567-569c1f480d29"
-                },
-                iOS: {
-                    cls: "iOS",
-                    text: "iOS",
-                    appLink: "https://itunes.apple.com/app/linphone/id360065638?mt=8"
-                },
-                android: {
-                    cls: "android",
-                    text: "android",
-                    appLink: "https://play.google.com/store/apps/details?id=org.linphone"
-                }
-            },
-            locales: [{
-                name: "US",
-                title: "English",
-                locale: "en_US"
-            }, {
-                name: "FR",
-                title: "Français",
-                locale: "fr_FR"
-            }],
-            models: {
-                contacts: new linphone.models.contacts.core.engine(a, d),
-                history: new linphone.models.history.core.engine(a, d)
-            },
-            checkOutdated: !0,
-            disableChat: !1,
-            disableChatFileTransfert: !1,
-            disableConference: !0,
-            disablePresence: !1
-        };
-    e.debug = d, e.logs = d, ("1" === jQuery.getUrlVar("lpdebug") || "true" === jQuery.getUrlVar("lpdebug") || "yes" === jQuery.getUrlVar("lpdebug")) && (e.logs = !0), "undefined" != typeof jQuery.getUrlVar("mimetype") && (e.mimetype = jQuery.getUrlVar("mimetype"), e.checkOutdated = !1), linphone.ui.init(a, e), linphone.ui.core.load(a)
-}! function(a, b, c) {
-    function d() {
-        function a(a, b) {
-            var c = document.createElement("script");
-            c.src = a, c.type = "text/javascript";
-            var d = document.getElementsByTagName("head")[0],
-                e = !1;
-            c.onload = c.onreadystatechange = function() {
-                e || this.readyState && "loaded" != this.readyState && "complete" != this.readyState || (e = !0, b(), c.onload = c.onreadystatechange = null, d.removeChild(c))
-            }, d.appendChild(c)
-        }
 
-        function b(c) {
-            if (e.length > 0) {
-                var d = e[0];
-                e.shift(), a(d.url, function() {
-                    b(c)
-                })
-            } else c()
-        }
-        b(function() {
-            jQuery(".linphoneweb").each(function(a) {
-                var b = linphone.ui.getBase(jQuery(this));
-                __linphone_init(b)
-            })
-        })
-    }
-    var e = [{
-        url: "http://web.linphone.org/js/jquery-1.10.1.min.js"
-    }, {
-        url: "http://web.linphone.org/js/jquery.client.min.js"
-    }, {
-        url: "http://web.linphone.org/js/analytics.min.js"
-    }, {
-        url: "http://web.linphone.org/js/i18n.min.js"
-    }, {
-        url: "http://web.linphone.org/js/persistent.min.js"
-    }, {
-        url: "http://web.linphone.org/js/jsonsql-0.1.min.js"
-    }, {
-        url: "http://web.linphone.org/js/jquery-ui-1.10.3.min.js"
-    }, {
-        url: "http://web.linphone.org/js/jquery.mousewheel.min.js"
-    }, {
-        url: "http://web.linphone.org/js/vertical.slider.min.js"
-    }, {
-        url: "http://web.linphone.org/js/jquery.geturlvars.min.js"
-    }, {
-        url: "http://web.linphone.org/js/jquery.watermark.min.js"
-    }, {
-        url: "http://web.linphone.org/js/handlebars.runtime.min.js"
-    }, {
-        url: "http://web.linphone.org/js/sha1.min.js"
-    }, {
-        url: "http://web.linphone.org/js/linphone-1.1.13.min.js"
-    }];
-    a.addEventListener ? a.addEventListener("load", d, !1) : a.attachEvent && a.attachEvent("onload", d)
-}(window, document, "script");
-			
 /* Method to create callbacks */
 function addEvent(obj, name, func){
 	var browser = searchString(getDataBrowser());
